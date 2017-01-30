a94c4b8a43b829c9c51eec4e0b8eb83f6a9473ae
Ruiferr <rui.diogo66@gmail.com>
2017-01-30 11:04:54 +0000

    basic layout (HTML, CSS, Jquery)

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.search').fadeIn(4000);
+	$('.gamestore').fadeIn(4000);
+	$('header').fadeIn(1500);
+	$('.line').animate({width:'toggle'},2000, function(){
+		$('.destaque').fadeIn(800);
+		$('.frame2').fadeIn(800);
+		$('.menu').fadeIn(800);
+	});
+
+
+})
